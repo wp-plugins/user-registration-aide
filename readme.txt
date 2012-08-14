343ac0ec074e7c186abfd3d67c67894efe3f879b
bnovotny <bnovotny@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-14 20:30:09 +0000

    git-svn-id: https://plugins.svn.wordpress.org/user-registration-aide/tags/1.0.2@585524 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,109 @@
+﻿=== User Registration Aide ===
+Contributors: Brian Novotny
+Donate link: http://creative-software-design-solutions.com/
+Author URI: http://creative-software-design-solutions.com/
+Plugin URI: http://creative-software-design-solutions.com/wordpress-user-registration-aide-force-add-new-user-fields-on-registration-form/
+Tags: user, registration, force, field, register, name, nickname, AIM, AOL, instant messenger, Yahoo, Jabber, Google Talk, Facebook, wordpress, plugin, extra, add, new, fields, profile
+Requires at least: 2.5
+Tested up to: 3.4.1
+Stable tag: 1.0.2
+
+Adds new fields and forces new users to register additional fields (such as first name and last name) or any new field you wish to add.
+
+Note: WordPress 2.5 or higher is definite required due to new error handling procedures.
+
+== Description ==
+
+Forces new users to register additional fields. Available fields are:
+
+*	First Name
+*	Last Name
+*	Nickname
+*	AIM screen name
+*	Yahoo IM screen name
+*	Jabber/Google Talk user name
+
+Wordpress User Registration Aide Force & Add New User Fields on Registration Form, as the title implies, allows you to force or require more fields when a new user registers. This not only can help to stop spammers, but it can also increase your user management capabilities and services. All the new fields that you add will also be added to existing users profiles, but the users will have to fill them out of course, but any new users will be required to fill out these fields.
+
+Another important option is that you can also add new fields but not require them for registration, so you can increase you user management capabilities. This is an exciting new feature for Webmasters that wish to increase contact options, communications, and information obtained from your user base.
+
+We are also currently working on additional features which will come out as a new plugin in the near future, however we released this simple version for those who want to add these simple features now to increase security and user information and reduce spammers. We originally got the idea from the Force User Field Registration from fergcorp, with their plugin, however that plugin hasn’t been updated in ages so we applied new functionality and replaced deprecated functions and added some new features and looks.
+
+As we stated earlier, Force User Field Registration was simple, and we kept it the same for the most part in that aspect, simple, easy to use and understand, even for the most computer illiterate.
+
+Plugin Features:
+
+    Easy to use
+    Quickly and easily add new fields to user registration
+    Add your own custom fields
+    Add as many or as little of the new fields to the registration form
+    Reduce bots and spammers with additional fields on the registration form
+    Includes existing fields from user profile like First Name, Last Name & Nickname
+    Add as many new fields you want quickly and easily
+    Get better control over your user base
+    New fields are added to existing users profiles!
+    Increase your knowledge of, and interaction with your customers & users!
+
+
+Read more: http://creative-software-design-solutions.com/wordpress-user-registration-aide-force-add-new-user-fields-on-registration-form/#ixzz22CCABfOx
+
+
+
+== Installation ==
+
+Download the file and put it in your plugins directory.
+
+Activate it.
+
+Method 2 (WordPress Add New Plugin):
+
+    Go to ‘add new’ menu under ‘plugins’ tab in your word-press admin.
+    Search ‘User Registration Aide’ plugin using search option.
+    Find the plugin and click ‘Install Now’ link.
+    Finally click activate plug-in link to activate the plug-in.
+
+[See Installation Instruction and Configuration information and Demo](Read more: http://creative-software-design-solutions.com/wordpress-user-registration-aide-force-add-new-user-fields-on-registration-form/#ixzz22CDaarHi)	
+
+
+To configure what fields are required for new users to register an account, login to the WordPress Dashboard. Then go to the Dashboard Home Screen -> Administration → Users → User Registration Aide
+
+Add Additional Fields to User Registration Form Option:
+
+This section includes a drop down box with all the existing fields in the WordPress profile, except bio, and all the additional new fields that you add, if any. None of them will show up on the registration form until you click the field you want to add to the registration form with your mouse.
+
+Read more: http://creative-software-design-solutions.com/wordpress-user-registration-aide-force-add-new-user-fields-on-registration-form/#ixzz22CEi4lRR
+
+== Frequently Asked Questions ==
+
+[Frequently Asked Questions](http://creative-software-design-solutions.com/wordpress-user-registration-aide-force-add-new-user-fields-on-registration-form/#axzz228bHzpkj)			
+
+
+== Screenshots ==
+
+1. User Registration Aide Admin Page Screenshot: http://creative-software-design-solutions.com/wp-content/uploads/2012/07/user-registration-aide-admin-page.jpg
+
+2. Administration Page Link: http://creative-software-design-solutions.com/wp-content/uploads/2012/07/admin-page-location.jpghttp://creative-software-design-solutions.com/wp-content/uploads/2012/07/admin-page-location.jpg
+
+3. Registration Form Screenshot: http://creative-software-design-solutions.com/wp-content/uploads/2012/07/registration-form-screenshot.jpg
+
+4. User Profile Screenshot: http://creative-software-design-solutions.com/wp-content/uploads/2012/07/user-profile-screenshot.jpg
+
+== Changelog ==	
+
+1.0.0		
+	
+First version. Final 7/31/2012
+
+New improved version for Force Users Field Registrations with some added features. First release stable, has add new field options, add existing and new fields to user registration, adds all new fields to current users profile pages, and gives you the option to require or not require when new members signup.
+
+1.0.1		
+	
+Second version. Final 8/1/2012
+
+Added a few updates to looks, more importantly fixed profile update bug so it works with new fields now and updates them properly. Also added filters to new user registrations for new fields.
+
+1.0.2
+
+Third Version 8/1/2012 9 PM EST
+
+Fixed bug of open line between PHP stop and start points which created header errors
---
+++ user-reg-aide-admin.php
@@ -0,0 +1,232 @@
+<?php
+
+/*
+ * 	User Registration Aide
+ * Plugin URI: http://creative-software-design-solutions.com/wordpress-user-registration-aide-force-add-new-user-fields-on-registration-form/
+ * Plugin Description: Forces new users to register additional fields with the option to add additional fields other than those 
+ * supplied with the default Wordpress Installation. We have kept it simple in this version for those of you whom aren't familiar with 
+ * handling multiple users or websites. We also are currently working on expanding this project with a paid version which will contain 
+ * alot more features and options for those of you who wish to get more control over users and user access to your site.
+ * Version: 1.0.2
+ * Author: Brian Novotny
+ * Author URI: http://creative-software-design-solutions.com/
+*/
+
+/**
+ * Fills array of known fields
+ *
+ * @since 1.0.0
+ * @access private
+ * @author Brian Novotny
+ * @website http://creative-software-design-solutions.com
+*/
+
+function fill_known_fields()
+{
+
+	$csds_userRegAide_knownFields = array(
+	"first_name"	=> "First Name",
+	"last_name"		=> "Last Name",
+	"nickname"		=> "Nickname",
+	"aim"			=> "AIM",
+	"yim"			=> "Yahoo IM",
+	"jabber"		=> "Jabber / Google Talk"
+	);
+	
+	update_option("csds_userRegAideFields", $csds_userRegAide_knownFields);
+	update_option("csds_userRegAide_knownFields", $csds_userRegAide_knownFields);
+	
+	csds_userRegAide_myOptionsSubpanel();
+	
+}
+
+/**
+ * Loads and displays the User Registration Aide administration page
+ *
+ * @since 1.0.0
+ * @access private
+ * @author Brian Novotny
+ * @website http://creative-software-design-solutions.com
+*/
+
+function csds_userRegAide_myOptionsSubpanel(){
+
+global $csds_userRegAide_knownFields, $seperator, $csds_userRegAide_getOptions, $csds_userRegAide_NewFields, $csds_userRegAideFields_getOptions, $csds_userRegMod_fields_missing, $csds_userRegMod_fields_missing1, $csds_userRegMod_fields_missing2;
+	
+	$csds_userRegAideFields = get_option('csds_userRegAideFields');
+	$csds_userRegAide_getOptions = get_option('csds_userRegAide');
+	$csds_userRegAide_knownFields = get_option('csds_userRegAide_knownFields');
+	$csds_userRegAide_NewFields = get_option('csds_userRegAide_NewFields');
+	$csds_userRegAide_support = get_option('csds_userRegAide_support');
+		if (isset($_POST['info_update']))
+		{
+			$results = array(	"additionalFields" => $_POST['additionalFields']);
+			update_option("csds_userRegAide", $results);
+			echo '<div id="message" class="updated fade"><p>'. __('Registration Form New Required Field Options updated successfully.', 'csds_userRegAide') .'</p></div>';					//Report to the user that the data has been updated successfully
+		}
+		
+		else
+		{
+		
+		}
+		
+		if (isset($_POST['field_update']))
+		{
+			if($_POST['csds_userRegAide_newFieldKey'] == '' || $_POST['csds_userRegAide_newField'] == ''){
+				if($_POST['csds_userRegAide_newFieldKey'] == '') {
+					$csds_userRegMod_fields_missing1 = " New Field Key ";
+				}
+				else{
+					
+				}
+				if($_POST['csds_userRegAide_newField'] == ''){
+					$csds_userRegMod_fields_missing2 = " New Field Name ";
+				}
+				else{
+					
+				}
+				if($_POST['csds_userRegAide_newFieldKey'] == '' && $_POST['csds_userRegAide_newField'] == ''){
+					$seperator = " & ";
+				}
+				else{
+					$seperator = " ";
+				}
+				$csds_userRegMod_fields_missing = $csds_userRegMod_fields_missing1 . $seperator . $csds_userRegMod_fields_missing2;
+			echo '<div id="message" class="updated fade"><p>'. __('Options not updated successfully. Missing following: '.$csds_userRegMod_fields_missing,'csds_userRegAide') .'</p></div>';
+			}
+			else{
+				$results = esc_attr(stripslashes($_POST['csds_userRegAide_newFieldKey']));
+				csds_add_field_to_users_meta($results);
+				$new_field = esc_attr(stripslashes($_POST['csds_userRegAide_newField']));
+				update_option("csds_userRegAide_newField", $csds_userRegAide_newField );
+				$csds_userRegAide_knownFields[$results] = $new_field;
+				$csds_userRegAide_NewFields[$results] = $new_field;
+				update_option("csds_userRegAide_NewFields", $csds_userRegAide_NewFields);
+				update_option("csds_userRegAideFields", $csds_userRegAide_knownFields);
+				update_option("csds_userRegAide_knownFields", $csds_userRegAide_knownFields);
+				echo '<div id="message" class="updated fade"><p>'. __('Add New Fields Options updated successfully.', 'csds_userRegAide') .'</p></div>';
+			}
+				
+		
+		}
+		
+		else
+		{
+		
+		}
+		
+		if (isset($_POST['csds_userRegAide_support_submit']))
+		{
+			$csds_userRegAide_support = esc_attr(stripslashes($_POST['csds_userRegAide_support']));
+			$csds_display_link = 'http://creative-software-design-solutions.com';
+			$csds_display_name = 'Powered By Creative Software Design Solutions';
+			
+			update_option('csds_userRegAide_support', $csds_userRegAide_support );
+			update_option("csds_display_link", $csds_display_link );
+			update_option("csds_display_name", $csds_display_name );
+			echo '<div id="message" class="updated fade"><p>'. __('Support Options updated successfully.', 'csds_userRegAide') .'</p></div>';					//Report to the user that the data has been updated successfully
+		}
+		
+		else
+		{
+		
+		}
+		
+	$csds_userRegAide_getOptions = get_option('csds_userRegAide');
+	$csds_userRegAideFields_getOptions = get_option('csds_userRegAideFields');
+
+	if(!empty($csds_userRegAide_knownFields)){
+		
+	
+echo '<div id="wpbody">';
+	echo '<div class=wrap>';
+		echo '<form method="post" name="csds_userRegAide">';
+            echo '<h2>User Registration Aide: Force New User Registration Fields</h2>';
+            echo '<div id="poststuff">';       
+                echo '<div class="stuffbox">';
+                    echo '<h3>Add Additional Fields to Registration Form</h3>';
+                    echo '<div class="inside">';
+                        
+                    echo'<p>By default, Wordpress will only require an email address and username to register an account. Here, you can select additional fields required for registration.</p>';
+    
+                        echo '<select name="additionalFields[]" id="csds_userRegMod_Select" size="8" multiple style="height:100px">';
+                       
+                            foreach($csds_userRegAide_knownFields as $key => $value){
+                                if(in_array("$key", $csds_userRegAide_getOptions["additionalFields"])){
+                                    $selected = "selected=\"selected\"";
+                                }
+                                else{
+                                    $selected = NULL;
+                                }
+                                echo "<option value=\"$key\" $selected >$value</option>\r\n";
+                            }
+							                        
+						echo '</select>';
+						
+                    echo '<p>Hold "Ctrl" to select multiple options</p>';
+					echo '<br/>';
+						echo '<p>';
+						?>
+						<input type="button" name="selectNone" value="<?php _e('Select None', 'csds_userRegAide'); ?>" onClick="document.getElementById('csds_userRegMod_Select').options.selectedIndex=-1;"/>
+						<?php
+						echo '</p>';	
+					   	echo '<div class="submit"><input type="submit" name="info_update" value="Update Options",'.csds_userRegAide.'  /></div>';
+						echo '</div>';
+					echo '</div>';
+						echo '<div class="stuffbox">';
+						echo '<h3>Add New Field</h3>';
+						echo '<div class="inside">';
+						echo '<p>Field Key Name : <input  style="width: 150px;" type="text" title="Enter the database name for your field here, like dob for Date of Birth or full_name, use lower case letters and _ (underscores) ONLY! Keep it short and simple and relative to the field you are creating!" value="';
+						echo $csds_userRegAide_newFieldKey . '" name="csds_userRegAide_newFieldKey" id="csds_userRegAide_newFieldKey" /></p>';
+						echo '<p>Field Name : <input  style="width: 150px;" type="text" title="Enter the user friendly name for your field here, like Date of Birth for dob, ect. Keep it short & simple and relative to the field you are creating!" value="';
+						echo $csds_userRegAide_newField . '" name="csds_userRegAide_newField" id="csds_userRegAide_newField" /></p>';
+						echo '<input type="submit" name="field_update" value="Add Field",'.csds_userRegAide.'  />';
+						echo '</div>';
+						echo '</div>';
+						
+						echo '<div class="stuffbox">';
+						echo '<h3><a href="http://creative-software-design-solutions.com" target="_blank">Creative Software Design Solutions</a></h3>';
+						echo '<div class="inside">';
+						
+						?>
+						<p>Show Plugin Support: <input type="radio" id="csds_userRegAide_support" name="csds_userRegAide_support"  value="1" <?php
+						if ($csds_userRegAide_support == 1) echo 'checked' ;?> /> Yes
+						<input type="radio" id="csds_userRegAide_support" name="csds_userRegAide_support"  value="2" <?php
+						if ($csds_userRegAide_support == 2) echo 'checked' ; ?> /> No
+						<br/>
+						<br/>
+
+							<?php
+								echo '<input name="csds_userRegAide_support_submit" id="csds_userRegAide_support_submit" lang="publish" class="button-primary" value="Update" type="Submit" />';
+								
+								?>
+								<h2>Plugin configuration help</h2>
+								<ul>
+									<li><a href="http://creative-software-design-solutions.com/wordpress-user-registration-aide-force-add-new-user-fields-on-registration-form/" target="_blank">Plugin Page & Screenshots</a></li>
+								</ul>
+								<h2>Check official website</h2>
+								<ul>
+									<li><a href="http://creative-software-design-solutions.com/wordpress-user-registration-aide-force-add-new-user-fields-on-registration-form/" target="_blank">Check official website for live demo</a></li>
+									<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+							<input type="hidden" name="cmd" value="_s-xclick">
+							<input type="hidden" name="hosted_button_id" value="6BCZESUXLS9NN">
+							<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+							<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+							</form>
+
+								</ul>
+								<?php
+						
+						echo '</div>';
+					echo '</div>';
+				echo '</div>';   
+			echo '</form>';
+		echo '</div>';
+	echo '</div>';
+	 }
+	else{
+		fill_known_fields();
+	}
+}
+
+?>
