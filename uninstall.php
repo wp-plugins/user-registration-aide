09b1c4dbced396856d980be1ec603a30b4e7de1d
bnovotny <bnovotny@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-18 17:38:52 +0000

    git-svn-id: https://plugins.svn.wordpress.org/user-registration-aide/tags/1.1.3@587198 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ uninstall.php
@@ -0,0 +1,43 @@
+<?php
+
+//For Debugging and Testing Purposes ------------
+
+//error_reporting(E_ALL);
+//ini_set('display_errors', '1');
+//$ebitd = ini_get('error_reporting');
+//error_reporting($ebits ^ E_NOTICE);
+
+// ----------------------------------------------
+
+	global $wpdb;
+	require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+	if(!defined('ABSPATH') && !defined('WP_UNINSTALL_PLUGIN')){
+		exit('Something Bad Happened');
+	}else{
+		
+		delete_option('csds_userRegAideFields');
+		delete_option('csds_userRegAide_knownFields');
+		delete_option('csds_userRegAide_fieldOrder');
+		delete_option('csds_userRegAide_registrationFields');
+		delete_option('csds_userRegAide_newField');
+		delete_option('csds_userRegAide_dbVersion');
+		delete_option('csds_userRegAide');
+		$csds_userRegAide_NewFields = get_option('csds_userRegAide_NewFields');
+			foreach($csds_userRegAide_NewFields as $field => $value){
+				
+				$count = $wpdb->get_var("SELECT COUNT(ID) FROM $wpdb->users;");
+				$i = 1;
+					while($i <= $count)
+				{
+					$user_id = $i;
+					delete_user_meta( $user_id, $field, "");
+					$i++;
+				}
+			}
+		delete_option('csds_userRegAide_NewFields');
+	
+	
+	}
+		
+	
+	?>
