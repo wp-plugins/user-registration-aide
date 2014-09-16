f656a155de6c584956cf823950f6da2f5ad8c549
bnovotny <bnovotny@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-16 21:30:51 +0000

    update 1.5.0.1
    git-svn-id: https://plugins.svn.wordpress.org/user-registration-aide/tags/1.5.0.0@991430 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/registration_complete.js
@@ -0,0 +1,9 @@
+jQuery(document).ready( function($) {
+
+	
+	var message = $('#message').html();
+	if ($('#message') == "Registration complete. Please check your e-mail."){
+		($('#message') == "REgistration Completed This is my message!!")
+	}
+	
+});
